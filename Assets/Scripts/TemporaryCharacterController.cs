209b0e91a76dfc61b6794e8aee900f3c3225ea62
Reveaper <dieterboddin95@hotmail.com>
2021-03-04 00:08:08 +0100

    Added Mixamo placeholder character
    [ADDED]
    - Mixamo xbot character model
    - Several Mixamo animations
    - Simple script to test the character's Animator
    
    [REMOVED]
    - URP ExampleAssets

---
+++ Assets/Scripts/TemporaryCharacterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class TemporaryCharacterController : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = this.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float movement = Input.GetKey(KeyCode.W) ? Time.deltaTime : -Time.deltaTime;
+        _animator.SetFloat("Forward", Mathf.Clamp(_animator.GetFloat("Forward") + movement, 0, 1));
+    }
+}
